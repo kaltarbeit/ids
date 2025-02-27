98695bef42f7e71a3314595adde51e99ec3fd4a0
langerhans <try9810@gmail.com>
2025-02-27 12:52:46 +0900

    drawer 완료

--- stories/Drawer.stories.js
+++ stories/Drawer.stories.js
@@ -1,4 +1,5 @@
 import { createDrawer as createDrawerFull } from '../components/drawer/full';
+import { createDrawer as createDrawerModal } from '../components/drawer/modal';
 
 export default {
     title: 'Components/Drawer',
@@ -7,4 +8,8 @@ export default {
 
 export const 풀_팝업형 = {
     render: () => createDrawerFull(),
+}
+
+export const 모달형 = {
+    render: () => createDrawerModal(),
 }
