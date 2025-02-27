d7e10afc7ffebc788ce8d6ba887cea1b84bf7144
langerhans <try9810@gmail.com>
2025-02-27 13:33:24 +0900

    dropdown 추가

---
+++ stories/Dropdown.stories.js
@@ -0,0 +1,10 @@
+import { createLabel as createLabelInfo } from '../components/label/info';
+
+export default {
+    title: 'Components/Label',
+    tags: ['autodocs'],
+};
+
+export const Default = {
+    render: () => createLabelInfo(),
+}
