e6ba968bba894f6283d008bf86e173eec3b12a7f
langerhans <langerhans@kaltarbeit.com>
2025-03-05 15:55:43 +0900

    infoBox 추가

---
+++ stories/InfoBox.stories.js
@@ -0,0 +1,20 @@
+import { createInfoBox as createInfoBoxDefault } from '../components/infoBox/default';
+import { createInfoBox as createInfoBoxWithButton } from '../components/infoBox/withButton';
+import { createInfoBox as createInfoBoxText } from '../components/infoBox/text';
+
+export default {
+    title: 'Components/InfoBox',
+    tags: ['autodocs'],
+};
+
+export const Default = {
+    render: () => createInfoBoxDefault(),
+}
+
+export const WithButton = {
+    render: () => createInfoBoxWithButton(),
+}
+
+export const Text = {
+    render: () => createInfoBoxText(),
+}
