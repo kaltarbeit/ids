436507710e27f23f35d2066dcd9a2970b81691de
langerhans <langerhans@kaltarbeit.com>
2025-03-10 18:55:30 +0900

    Pagination 추가

---
+++ stories/Pagination.stories.js
@@ -0,0 +1,20 @@
+import { createPagination } from '../components/pagination/pagination';
+import { createIndicator } from '../components/pagination/indicator';
+import { createMore } from '../components/pagination/more';
+
+export default {
+    title: 'Components/Pagination',
+    tags: ['autodocs'],
+};
+
+export const Pagination = {
+    render: () => createPagination(),
+}
+
+export const Indicator = {
+    render: () => createIndicator(),
+}
+
+export const LoadMore = {
+    render: () => createMore(),
+}
