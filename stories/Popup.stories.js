9528b2f21fb45ba852094fdffbf006dfd3cf80b2
langerhans <try9810@gmail.com>
2025-03-11 03:14:39 +0900

    popup 작업중

---
+++ stories/Popup.stories.js
@@ -0,0 +1,10 @@
+import { createPopup as createPopupLayer  } from '../components/popup/layer';
+
+export default {
+    title: 'Components/Popup',
+    tags: ['autodocs'],
+};
+
+export const Default = {
+    render: () => createPopupLayer(),
+}
