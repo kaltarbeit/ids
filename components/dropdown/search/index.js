8968ddf9bf9c9a41f7c916964c1c4e1df9fc0f9a
langerhans <try9810@gmail.com>
2025-03-31 08:10:48 +0900

    dropdown 추가

---
+++ components/dropdown/search/index.js
@@ -0,0 +1,35 @@
+import html from '!!raw-loader!./index.html';
+
+
+export const createDropdown = () => {
+    const wrapper = document.createElement('div');
+    wrapper.insertAdjacentHTML('afterbegin', html);
+
+    const textField = wrapper.querySelector('.ids2-text-field');
+    const textFieldInput = wrapper.querySelector('.ids2-text-field input');
+    const clearButton = wrapper.querySelector('.ids2-text-field .ids2-input-clear');
+    const counter = wrapper.querySelector('.ids2-text-field-counter');
+
+    textFieldInput.addEventListener('input', (e) => {
+        const count = e.target.value.length;
+        count > 0 ? textField.classList.add('ids2-text-field-filled') : textField.classList.remove('ids2-text-field-filled');
+
+        if(counter) {
+            const countWrapper = counter.querySelector('.ids2-text-field-count');
+            countWrapper.textContent = count;
+        }
+    });
+
+    clearButton.addEventListener('click', () => {
+        textFieldInput.value = '';
+        textField.classList.remove('ids2-text-field-filled');
+        textFieldInput.focus();
+    });
+
+    if(counter) {
+        const maxLength = counter.querySelector('.ids2-text-field-maxlength');
+        maxLength.textContent = textFieldInput.maxLength;
+    }
+
+    return wrapper.firstElementChild;
+}
--- stories/Dropdown.stories.js
+++ stories/Dropdown.stories.js
@@ -1,4 +1,12 @@
 import { createDropdown as createDropdownDefault } from '../components/dropdown/default';
+import { createDropdown as createDropdownEmail } from '../components/dropdown/email';
+import { createDropdown as createDropdownSearch } from '../components/dropdown/search';
+import { createDropdown as createDropdownBank } from '../components/dropdown/bank';
+import { createDropdown as createDropdownBankIcon } from '../components/dropdown/bankIcon';
+import { createDropdown as createDropdownBankSelect } from '../components/dropdown/bankSelect';
+import { createDropdown as createDropdownSmall } from '../components/dropdown/small';
+import { createDropdown as createDropdownText } from '../components/dropdown/text';
+import { createDropdown as createDropdownTextSmall } from '../components/dropdown/textSmall';
 
 export default {
     title: 'Components/Dropdown',
@@ -7,4 +15,36 @@ export default {
 
 export const Default = {
     render: () => createDropdownDefault(),
+}
+
+export const Email = {
+    render: () => createDropdownEmail(),
+}
+
+export const Search = {
+    render: () => createDropdownSearch(),
+}
+
+export const Bank = {
+    render: () => createDropdownBank(),
+}
+
+export const BankIcon = {
+    render: () => createDropdownBankIcon(),
+}
+
+export const BankSelect = {
+    render: () => createDropdownBankSelect(),
+}
+
+export const Small = {
+    render: () => createDropdownSmall(),
+}
+
+export const Text1 = {
+    render: () => createDropdownText(),
+}
+
+export const Text2 = {
+    render: () => createDropdownTextSmall(),
 }
