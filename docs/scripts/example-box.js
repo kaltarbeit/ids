aeb217d2af5989ba64e42b1b2fd60868162a8c88
langerhans <langerhans@kaltarbeit.com>
2025-04-28 19:07:09 +0900

    docs 작업중

--- docs/scripts/example-box.js
+++ docs/scripts/example-box.js
@@ -1,17 +1,8 @@
-document.querySelectorAll('.ids2-docs-examples').forEach(function (example) {
+document.querySelectorAll('.ids2-docs-example-code').forEach(function (example) {
 
+    const code = example.querySelector('.language-markup');
 
-    example.querySelectorAll('.ids2-docs-example-code').forEach(function (box) {
-
-        const pre = document.createElement('pre');
-        const code = document.createElement('code');
-        const text = document.createTextNode(box.querySelector('.ids2-docs-example').innerHTML);
-        code.appendChild(text);
-
-        code.className = 'language-markup';
-        pre.appendChild(code);
-        pre.className = 'ids2-docs-example-code-highlight';
-
-        box.appendChild(pre);
-    });
+    if(code) {
+        example.querySelector('.ids2-docs-example').innerHTML = code.innerHTML;
+    }
 });
