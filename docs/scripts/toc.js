c365e6682d57a0e32d4e773b0439953383d3e048
langerhans <try9810@gmail.com>
2025-04-28 10:48:29 +0900

    문서 작업중

---
+++ docs/scripts/toc.js
@@ -0,0 +1,8 @@
+
+const toc = document.createElement('ul');
+toc.classList.add('ids2-docs-toc');
+
+document.querySelectorAll('.ids2-docs-section-title').forEach(el => {
+
+    toc.appendChild();
+});
