3dfb1e448c3ad167b5584c4cde2a5961c375d2b0
langerhans <langerhans@kaltarbeit.com>
2025-05-20 14:42:11 +0900

    스토리북 정리

--- components/bottomNavigation/floating/index.js
+++ components/bottomNavigation/floating/index.js
@@ -5,12 +5,12 @@ export const createBottomNavigation = () => {
     const wrapper = document.createElement('div');
     wrapper.insertAdjacentHTML('afterbegin', html);
 
-    const navItems = wrapper.querySelectorAll('.nav-item');
+    const navItems = wrapper.querySelectorAll('.ids-bottom-nav-item');
 
     navItems.forEach(item => {
         item.addEventListener('click', () => {
-            navItems.forEach(item => item.classList.remove('active'));
-            item.classList.add('active');
+            navItems.forEach(item => item.classList.remove('ids-bottom-nav-item-active'));
+            item.classList.add('ids-bottom-nav-item-active');
         })
     });
 
